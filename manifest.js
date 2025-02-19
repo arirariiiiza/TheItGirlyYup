14f2ef039ae2d4ae28078f0e04d9b53f0ea273cf
arirariiiiza <iravenny23@gmail.com>
2025-02-19 15:24:59 -0500

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,12 @@
+{
+  "display_name": "TheItGirlyYup",
+  "loading_order": 1,
+  "requires": [],
+  "optional": [],
+  "js": "index.js",
+  "css": "",
+  "author": "rari",
+  "version": "1.0.0",
+  "homePage": "https://github.com/arirariiiiza/TheItGirlyYup",
+  "auto_update": true
+}
